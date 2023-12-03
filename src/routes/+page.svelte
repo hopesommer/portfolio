9255cdd79a1d1ea4d44a3564c96eefcc24a2e185
Hope Miner <hopeminer@Hopes-MacBook-Air.local>
2023-12-03 00:44:05 -0700

    lil bb change

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,14 +10,8 @@
 </svelte:head>
 
 <section class="flex flex-col justify-center mt-12">
-	<!-- <div class="relative mb-7">
-		<div class="z-50 absolute flex label font-medium rounded-lg w-[200px] mt-3 p-4 bg-sage block font-mondeur justify-center">Software Engineer</div>
-		<div class="flex justify-center mr-12">
-			<img class="profile" alt="me" src="/images/profile.png"/>
-		</div>
-	</div> -->
 	<img class="decorative self-center" alt="decorative" src="/images/composition_4.png"/>
-	<h1 class="block font-brightMelody text-center text-9xl text-antique-white">Hope Sommer</h1>
+	<h1 class="block font-brightMelody text-center text-8xl text-antique-white">Hope Sommer</h1>
 	<h3 class="mt-2 flex justify-center font-mondeur text-pink text-3xl">Software Engineer</h3>
 	<div class="mt-9 bg-citron self-center rounded-lg p-4 text-dark-green font-mondeur">Download CV</div>
 </section>
