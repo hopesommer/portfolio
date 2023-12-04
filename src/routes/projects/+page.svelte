59c95d580ef5ef1ae1b1b9650af431134c7f4746
Hope Miner <hopeminer@Hopes-MacBook-Air.local>
2023-12-03 17:56:34 -0700

    Projects

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -4,5 +4,22 @@
 </svelte:head>
 
 <div class="text-column flex justify-center mt-9">
-	<span class="text-3xl text-antique-white font-brightMelody">Under Construction</span>
+	<span class="text-5xl text-antique-white font-brightMelody">Projects</span>
+	<!-- <a href="https://pokedex-snr.vercel.app/" class="flex">
+		<div class="card bg-base-100 shadow-xl relative">
+		<figure class="projectImage"><img class="projectImage" src="/images/pokedex.png" alt="Shoes" /></figure>
+		<div class="card-body">
+			<h2 class="card-title">
+				Pokedex
+			<div class="badge badge-secondary white-text">NEW</div>
+			</h2>
+			<div class="card-actions justify-end">
+				<div class="badge badge-outline">graphQL</div> 
+				<div class="badge badge-outline">TypeScript</div>
+				<div class="badge badge-outline">React</div>
+			</div>
+		</div>
+	</div>
+	</a> -->
+	
 </div>
--- src/routes/resume/+page.svelte
+++ src/routes/resume/+page.svelte
@@ -5,5 +5,5 @@
 </svelte:head>
 
 <div class="text-column flex justify-center mt-9">
-	<span class="text-3xl text-antique-white font-brightMelody">Under Construction</span>
+	<span class="text-5xl text-antique-white font-brightMelody">Resume</span>
 </div>
